1992e255758f7b5ad2a135e243325599118fc039
jeon <maninblack87@naver.com>
2025-07-25 15:06:18 +0900

    20250725 1st

---
+++ 2nd/Event.py
@@ -0,0 +1,81 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+
+import Connect
+
+def on_tree_select(tree, std_id:tk.Entry, name:tk.Entry, email:tk.Entry, major:ttk.Combobox, state:ttk.Combobox, current_user, delete_button:tk.Button):
+    """
+    tree : Treeview에 의해 만틀어진 뷰
+    std_id,name,email,major,state : 학생의 데이터들의 입력칸
+    current_user : CurrentUser.json으로부터 불러온 사용자 정보
+    delete_button : 삭제 버튼
+    """
+
+    # 선택한 트리의 아이디 가져오기
+    selected_item = tree.focus()
+    # >> 예외
+    if not selected_item:
+        return
+
+    # 선택한 트리의 값만 가져오기
+    values = tree.item(selected_item, "values")
+    # >> 예외
+    if not values or len(values) < 4:
+        return
+
+    # 데이터베이스 연결
+    db_connection = Connect.connect_to_mysql()
+    
+    # 쿼리를 보낼 커서 생성
+    cursor = db_connection.cursor()
+
+    # SQL문으로 뷰를 생성 : 학생정보와 학과정보를 병합시킴
+    query = """
+        SELECT * 
+        FROM (SELECT A.학번, A.이름, A.이메일, B.명칭, A.상태 
+            FROM 학생정보 A JOIN 학과정보 B
+            WHERE A.학과 = B.학과코드) VIEW
+        WHERE VIEW.학번 = %s
+    """
+    cursor.execute(query, (values[1],))
+    selected_std_info = cursor.fetchone()
+
+    # 선택한 트리의 값을 입력란에 표시
+    # # 이때 각 입력란(Entry)는 **일시적으로** 활성화시켜야 됨
+    # 1) 학번
+    std_id.config(state="normal")
+    std_id.delete(0, tk.END)
+    std_id.insert(0, selected_std_info[0])
+    std_id.config(state="disabled")
+    # 2) 이름
+    name.config(state="normal")
+    name.delete(0, tk.END)
+    name.insert(0, selected_std_info[1])
+    name.config(state="disabled")
+    # 3) 이메일
+    email.config(state="normal")
+    email.delete(0, tk.END)
+    email.insert(0, selected_std_info[2])
+    email.config(state="disabled")
+    # 4) 학과(명칭)
+    major.config(state="normal")
+    major.delete(0, tk.END)
+    major.insert(0, selected_std_info[3])
+    major.config(state="disabled")
+    # 5) 상태
+    state.config(state="normal")
+    state.delete(0, tk.END)
+    state.insert(0, selected_std_info[4])
+    state.config(state="disabled")
+
+    # 사용자의 권한 정보에 따라 권한 부여/제한
+    # 사용자 권한이 USER
+    # >> 학생 추가 기능 제한 : 학생의 학번과 이름을 비활성화시킨다.
+    if current_user["role"] == 'user':
+        std_id.config(state="disabled")
+        name.config(state="disabled")
+    # 사용자 권한이 ADMIN
+    # >> 삭제 기능 부여
+    if current_user["role"] == 'admin':
+        delete_button.config(state="normal")
--- 2nd/t3.py
+++ 2nd/t3.py
@@ -1,11 +1,18 @@
 import tkinter as tk
 from tkinter import ttk
+import json
 
 import Connect
 from Query import searchStudentInfo
+from Event import on_tree_select
 
+# 데이터베이스 연결 및 (쿼리를 전송할) 커서 생성
 db_connection = Connect.connect_to_mysql()
 cursor = db_connection.cursor()
+
+# 로그인중인 사용자 정보를 JSON 파일에서 불러오기
+with open("CurrentUser.json", "r", encoding="utf-8") as f:
+    current_user = json.load(f)
 
 root = tk.Tk()
 root.title("학생관리")
@@ -68,10 +75,12 @@ frame3.pack(side="left", fill="both", padx=10, pady=20)
 # >> 프레임3-1
 frame3_1 = tk.Frame(frame3, pady=10)
 frame3_1.pack(side="top", anchor="w")
+# >> >> 학번
 label4 = tk.Label(frame3_1, text="학번", width=5)
 label4.pack(side="left")
 entry4 = tk.Entry(frame3_1, width=12)
 entry4.pack(side="left", padx=20)
+# >> >> 이름
 label5 = tk.Label(frame3_1, text="이름", width=5)
 label5.pack(side="left")
 entry5 = tk.Entry(frame3_1, width=12)
@@ -79,6 +88,7 @@ entry5.pack(side="left")
 # >> 프레임3-2
 frame3_2 = tk.Frame(frame3, pady=10)
 frame3_2.pack(side="top", anchor="w")
+# >> >> 이메일
 label6 = tk.Label(frame3_2, text="이메일", width=5)
 label6.pack(side="left")
 entry6 = tk.Entry(frame3_2, width=32)
@@ -91,15 +101,42 @@ majors = [row[0] for row in cursor.fetchall()]
 majors.insert(0, "선택")
 frame3_3 = tk.Frame(frame3, pady=10)
 frame3_3.pack(side="top", anchor="w")
+# >> >> 학과
 label7 = tk.Label(frame3_3, text="학과", width=5)
 label7.pack(side="left")
 combo7 = ttk.Combobox(frame3_3, value=majors, state="readonly", width=15)
 combo7.pack(side="left")
+# >> >> 상태
 label8 = tk.Label(frame3_3, text="상태")
 label8.pack(side="left")
-# >> >> 학생 상태 조회 쿼리
+# >> >> >> 학생 상태 조회 쿼리
 status = ["재학", "졸업", "휴학", "퇴학"]
 combo8 = ttk.Combobox(frame3_3, values=status, state="readonly", width=8)
 combo8.pack(side="left")
+
+# >> 프레임 3-4
+frame3_4 = tk.Frame(frame3, pady=10)
+frame3_4.pack(side="bottom", anchor="w")
+# >> >> 추가 버튼
+add_btn = tk.Button(frame3_4, text="추가")
+add_btn.pack(side="left")
+# >> >> 저장 버튼
+save_btn = tk.Button(frame3_4, text="저장")
+save_btn.pack(side="left")
+# >> >> 삭제 버튼
+delete_btn = tk.Button(frame3_4, text="삭제")
+delete_btn.pack(side="left")
+
+# 권한에 따른 권한 부여/제한
+# >> 사용자가 USER일 경우
+# >> >> 추가, 저장, 삭제 버튼 비활성화
+if (current_user["role"] == "user"):
+    add_btn.config(state="disabled")
+    save_btn.config(state="disabled")
+    delete_btn.config(state="disabled")
+
+# 이벤트 바인딩
+# >> Treeview에서 항목 선택시 이벤트 발생
+tree.bind("<<TreeviewSelect>>", lambda e: on_tree_select(tree, entry4, entry5, entry6, combo7, combo8, current_user, delete_btn))
 
 root.mainloop()
