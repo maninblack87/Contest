1e62d4212abc779acd9636239522ef583557285b
jeon <maninblack87@naver.com>
2025-08-08 16:32:39 +0900

    20250808 1st

--- 3rd/app.py
+++ 3rd/app.py
@@ -5,5 +5,12 @@ import t1
 # 데이터베이스 연결
 my_db = SQLiteDB.SQLiteDB()
 my_db.connect()
+
+# 데이터베이스에 각 테이블 추가 / 각 테이블에 레코드 추가
 my_db.execute_file("./mySQLite/DatabaseStructure.sql")
+
+# 데이터베이스 종료
 my_db.close()
+
+# t1.py 실행
+t1.main()
---
+++ 3rd/routes/Router.py
@@ -0,0 +1,9 @@
+# Router.py
+import os, sys
+
+
+
+if __name__ == "__main__":
+    print("__file__ : ", __file__)
+    print("os.path.abspath(__file__) : ", os.path.abspath(__file__))
+    print("os.path.dirname(os.path.abspath(__file__)) : ", os.path.dirname(os.path.abspath(__file__)))
