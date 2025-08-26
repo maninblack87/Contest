340fe1a9caf308c3d782e71b3d20c731eafc9182
jeon <maninblack87@naver.com>
2025-08-26 16:14:14 +0900

    20250826 3rd

--- 3rd/t3.py
+++ 3rd/t3.py
@@ -13,26 +13,100 @@ def main():
     # 루트 위젯
     root = tk.Tk()
     root.title("학생 관리")
-    root.geometry("600x400")
+    root.geometry("700x400")
     root.resizable(False, False)
-    root.option_add("*Font", "Gothic 10")
+    root.option_add("*Font", "Gothic 11")
 
     # 상단 프레임 : 학생 정보 목록을 검색하는 부분
-    frame_upper = tk.Frame(root, height=50, bg="#faa")
-    frame_upper.pack(side="top", fill="x")
-    label1 = tk.Label(frame_upper, text="학과", width=8)
-    label1.pack(side="left")
-    # >> 콤보박스
+    frame_top = tk.Frame(root, height=50)
+    frame_top.pack(side="top", fill="x", pady=10)
+    # >> 학과
     query1 = "select 명칭 from 학과정보"
     connect.cursor.execute(query1)
     result = connect.cursor.fetchall()
-    print(result)
+    majors = ["선택"]
+    for i in range(len(result)):
+        majors.append(result[i][0])
+    label1 = tk.Label(frame_top, text="학과", width=8)
+    label1.pack(side="left")
+    combo1 = ttk.Combobox(frame_top, values=majors, state="readonly", width=15)
+    combo1.pack(side="left")
+    combo1.current(0)
+    # >> 학번
+    label2 = tk.Label(frame_top, text="학번", width=8)
+    label2.pack(side="left")
+    entry2 = tk.Entry(frame_top, width=15)
+    entry2.pack(side="left")
+    # >> 이름
+    label3 = tk.Label(frame_top, text="이름", width=8)
+    label3.pack(side="left")
+    entry3 = tk.Entry(frame_top, width=15)
+    entry3.pack(side="left")
+    # >> 검색 버튼
+    button4 = tk.Button(frame_top, text="검색", width=8)
+    button4.pack(side="left", padx=10)
 
     # 좌측 프레임 : 학생 정보 목록이 표시되는 부분
+    frame_left = tk.Frame(root, width=250, height=350, padx=10, pady=10)
+    frame_left.pack(side="left", fill="both", expand=True)
+    tree = ttk.Treeview(frame_left, columns=("이름", "학번", "학과", "상태"), show="headings")
+    # 테이블
+    tree.heading("이름", text="이름")
+    tree.heading("학번", text="학번")
+    tree.heading("학과", text="학과")
+    tree.heading("상태", text="상태")
+    # 테이블 너비
+    tree.column("이름", width=80)
+    tree.column("학번", width=80)
+    tree.column("학과", width=80)
+    tree.column("상태", width=60)
+    # 스크롤바
+    scrollbar = ttk.Scrollbar(frame_left, orient="vertical", command=tree.yview)
+    tree.configure(yscrollcommand=scrollbar.set)
+    # 테이블 배치
+    tree.pack(side="left", fill="both", expand=True)
+    scrollbar.pack(side="right", fill="y")
 
-    # 우측1 프레임 : 학생 정보를 입력하는 부분
+    # 우측 프레임 : 학생 정보를 입력하는 부분, 버튼 셋 부분
+    frame_right = tk.Frame(root, width=400, height=350, padx=10, pady=10)
+    frame_right.pack(side="right", fill="both", expand=True)
 
-    # 우측2 프레임 : 버튼 모음
+    # 우측1 프레임 : 학생 정보를 입력하는 부분(1)
+    frame_r1 = tk.Frame(frame_right, width=400, height=30, pady=5)
+    frame_r1.pack(side="top", anchor="nw")
+    # >> 학번
+    label5 = tk.Label(frame_r1, text="학번", width=5, anchor="nw")
+    label5.pack(side="left")
+    entry5 = tk.Entry(frame_r1, width=10)
+    entry5.pack(side="left", padx=5)
+    # >> 이름
+    label6 = tk.Label(frame_r1, text="이름", width=5, anchor="ne")
+    label6.pack(side="left")
+    entry6 = tk.Entry(frame_r1, width=10)
+    entry6.pack(side="left", padx=5)
+
+    # 우측2 프레임 : 학생 정보를 입력하는 부분(2)
+    frame_r2 = tk.Frame(frame_right, width=400, height=30, pady=5)
+    frame_r2.pack(side="top", anchor="nw")
+    # >> 이메일
+    label6 = tk.Label(frame_r2, text="이메일", width=5)
+    label6.pack(side="left")
+    entry6 = tk.Entry(frame_r2, width=28)
+    entry6.pack(side="left", padx=5)
+
+    # 우측3 프레임 : 학생 정보를 입력하는 부분(3) <-- ##### 작업중 #####
+    frame_r1 = tk.Frame(frame_right, width=400, height=30, pady=5)
+    frame_r1.pack(side="top", anchor="nw")
+    # >> 학번
+    label5 = tk.Label(frame_r1, text="학번", width=5, anchor="nw")
+    label5.pack(side="left")
+    entry5 = tk.Entry(frame_r1, width=10)
+    entry5.pack(side="left", padx=5)
+    # >> 이름
+    label6 = tk.Label(frame_r1, text="이름", width=5, anchor="ne")
+    label6.pack(side="left")
+    entry6 = tk.Entry(frame_r1, width=10)
+    entry6.pack(side="left", padx=5)
 
     # 루트로 GUI 활성화
     root.mainloop()
