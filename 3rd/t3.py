afd8aa04ef6c5a0298611884bd1a033cdd418d56
jeon <maninblack87@naver.com>
2025-08-27 16:37:21 +0900

    20250827 1st

--- 3rd/t3.py
+++ 3rd/t3.py
@@ -18,8 +18,8 @@ def main():
     root.option_add("*Font", "Gothic 11")
 
     # 상단 프레임 : 학생 정보 목록을 검색하는 부분
-    frame_top = tk.Frame(root, height=50)
-    frame_top.pack(side="top", fill="x", pady=10)
+    frame_top = tk.Frame(root, height=50, bg="#faa")
+    frame_top.pack(side="top", fill="both", ipady=10)
     # >> 학과
     query1 = "select 명칭 from 학과정보"
     connect.cursor.execute(query1)
@@ -46,8 +46,12 @@ def main():
     button4 = tk.Button(frame_top, text="검색", width=8)
     button4.pack(side="left", padx=10)
 
+    # 상/하단 구분선
+    hor_line = tk.Frame(root, bg="black")
+    hor_line.pack(side="top", fill="x")
+
     # 좌측 프레임 : 학생 정보 목록이 표시되는 부분
-    frame_left = tk.Frame(root, width=250, height=350, padx=10, pady=10)
+    frame_left = tk.Frame(root, width=250, height=350, padx=10, pady=10, bg="#afa")
     frame_left.pack(side="left", fill="both", expand=True)
     tree = ttk.Treeview(frame_left, columns=("이름", "학번", "학과", "상태"), show="headings")
     # 테이블
@@ -68,7 +72,7 @@ def main():
     scrollbar.pack(side="right", fill="y")
 
     # 우측 프레임 : 학생 정보를 입력하는 부분, 버튼 셋 부분
-    frame_right = tk.Frame(root, width=400, height=350, padx=10, pady=10)
+    frame_right = tk.Frame(root, width=400, height=350, padx=10, pady=10, bg="#aaf")
     frame_right.pack(side="right", fill="both", expand=True)
 
     # 우측1 프레임 : 학생 정보를 입력하는 부분(1)
@@ -95,18 +99,27 @@ def main():
     entry6.pack(side="left", padx=5)
 
     # 우측3 프레임 : 학생 정보를 입력하는 부분(3) <-- ##### 작업중 #####
-    frame_r1 = tk.Frame(frame_right, width=400, height=30, pady=5)
-    frame_r1.pack(side="top", anchor="nw")
+    frame_r3 = tk.Frame(frame_right, width=400, height=30, pady=5)
+    frame_r3.pack(side="top", anchor="nw")
     # >> 학번
-    label5 = tk.Label(frame_r1, text="학번", width=5, anchor="nw")
+    label5 = tk.Label(frame_r3, text="학번", width=5, anchor="nw")
     label5.pack(side="left")
-    entry5 = tk.Entry(frame_r1, width=10)
+    entry5 = tk.Entry(frame_r3, width=10)
     entry5.pack(side="left", padx=5)
     # >> 이름
-    label6 = tk.Label(frame_r1, text="이름", width=5, anchor="ne")
+    label6 = tk.Label(frame_r3, text="이름", width=5, anchor="ne")
     label6.pack(side="left")
-    entry6 = tk.Entry(frame_r1, width=10)
+    entry6 = tk.Entry(frame_r3, width=10)
     entry6.pack(side="left", padx=5)
+
+    # 우측4 프레임 : 버튼 모음 부분
+    frame_r4 = tk.Frame(frame_right, width=400, height=70, pady=5)
+    frame_r4.pack(side="bottom", anchor="nw")
+    btn_add = tk.Button(frame_r4, text="추가", width=5)
+    btn_add.pack(side="left", padx=5)
+    btn_save = tk.Button(frame_r4, text="저장", width=5)
+    btn_save.pack(side="left", padx=5)
+    btn_del = tk.Button(frame_r4, text="삭제", width=5)
 
     # 루트로 GUI 활성화
     root.mainloop()
